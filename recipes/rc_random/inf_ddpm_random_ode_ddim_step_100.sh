f5033e56f052e74d82891b1305752b53d899c424
Beilong Tang <btang5@ncsu.edu>
2025-08-28 14:58:00 -0400

    update training

--- recipes/rc_random/inf_ddpm_random_ode_ddim_step_100.sh
+++ recipes/rc_random/inf_ddpm_random_ode_ddim_step_100.sh
@@ -8,18 +8,20 @@ set -o pipefail
 
 # Sample
 echo "[sampling]"
-output_dir=output/DDPM_CIFAR10_EPS_RANDOM_400000_ode_ddim_step_100
-python sample.py \
+step=100
+output_dir=output/DDPM_CIFAR10_EPS_RANDOM_400000_ode_ddim_step_$step
+python sample_ddim.py \
     --flagfile logs/DDPM_CIFAR_10_EPS_RANDOM/flagfile.txt \
     --logdir logs/DDPM_CIFAR_10_EPS_RANDOM \
-    --batch_size 128 \
+    --batch_size 80 \
     --sample_img_from_noise_pair \
     --sample_img_noise_pair_path "$output_dir" \
     --num_images 50000 \
     --num_procs 5 \
-    --gpus cuda:3,cuda:4,cuda:5,cuda:6,cuda:7
+    --gpus cuda:3,cuda:4,cuda:5,cuda:6,cuda:7 \
+    --steps "$step"
 
-echo "[inferencing]s"
+echo "[inferencing]"
 # Inference
 CUDA_VISIBLE_DEVICES="7" python eval.py \
     --eval_output_dir "$output_dir/images_npy"
